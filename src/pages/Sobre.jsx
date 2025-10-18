7b946695c638d545f87804fc34a3184f958fe456
Diego Ruan <diego_ruan.14@hotmail.com>
2025-10-18 15:58:44 -0300

    Adicionando pagina Sobre

--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -1,13 +1,17 @@
 import useConfig from "../hooks/useConfig";
+import "./Sobre.css";
 
-export default function About() {
+export default function Sobre() {
   const config = useConfig();
   if (!config) return <div className="text-center mt-5">Carregando...</div>;
 
   return (
     <div className="container mt-5">
-      <h2>Quem Somos</h2>
-      <p className="mt-3">{config.sobre}</p>
+      <h3 className="titulo-custom">{config.sobre.titulo}</h3>
+      <div 
+        className="sobre-conteudo"
+        dangerouslySetInnerHTML={{ __html: config.sobre.conteudo }}
+      />
     </div>
   );
 }
